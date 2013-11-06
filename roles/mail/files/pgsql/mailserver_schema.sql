a109b61a98a5da30f01598299cb831eb147f9052
Benjamin Podszun <benjamin.podszun@gmail.com>
2013-11-06 16:30:03 +0100

    Add support for check_recipient_access records, to - for example - whitelist postmaster@ regardless of dspam/postgrey

--- roles/mail/files/pgsql/mailserver_schema.sql
+++ roles/mail/files/pgsql/mailserver_schema.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS virtual_domains (id SERIAL, name varchar(256) NOT NULL, PRIMARY KEY (id), UNIQUE (name));
 
-CREATE TABLE IF NOT EXISTS virtual_users (id SERIAL, domain_id int REFERENCES virtual_domains(id) ON DELETE CASCADE, username varchar(100) NOT NULL, password varchar(128) NOT NULL, quota_limit_bytes int DEFAULT 104857600, PRIMARY KEY (id), UNIQUE (domain_id,username));
+CREATE TABLE IF NOT EXISTS virtual_users (id SERIAL, domain_id int NOT NULL REFERENCES virtual_domains(id) ON DELETE CASCADE, username varchar(100) NOT NULL, password varchar(128) NOT NULL, quota_limit_bytes int DEFAULT 104857600, PRIMARY KEY (id), UNIQUE (domain_id,username));
 
-CREATE TABLE IF NOT EXISTS virtual_aliases (id SERIAL, domain_id int REFERENCES virtual_domains(id) ON DELETE CASCADE, username varchar(100) NOT NULL, destination varchar(100) NOT NULL, PRIMARY KEY (id), UNIQUE (domain_id,username));
+CREATE TABLE IF NOT EXISTS virtual_aliases (id SERIAL, domain_id int NOT NULL REFERENCES virtual_domains(id) ON DELETE CASCADE, username varchar(100) NOT NULL, destination varchar(100) NOT NULL, PRIMARY KEY (id), UNIQUE (domain_id,username));
+
+CREATE TABLE IF NOT EXISTS recipient_access (id SERIAL, address varchar(100) NOT NULL, action varchar(100) NOT NULL Default 'reject', PRIMARY KEY (id), UNIQUE (address));
 
 CREATE OR REPLACE VIEW dspam_virtual_uids AS SELECT u.Id, concat(u.username,'@',d.name) AS Email FROM virtual_users u LEFT JOIN virtual_domains d ON (u.domain_id = d.id);
